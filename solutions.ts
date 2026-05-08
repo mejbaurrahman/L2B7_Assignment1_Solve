497b3bb1b8024754b154ac4f82d8b3da4240c5b7
unknown <mejbacsedu@gmail.com>
2026-05-08 16:38:26 +0600

    solved

--- solutions.ts
+++ solutions.ts
@@ -61,7 +61,7 @@ class Student extends Person {
   }
 }
 
-// problem -7 solution
+// problem-7 solution
 
 function getIntersection<T>(arr1: T[], arr2: T[]): T[] {
   return arr1.filter((item) => arr2.includes(item));
